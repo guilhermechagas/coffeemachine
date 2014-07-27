9079927a913b5112962ff2aa3ccf17000565976c
Guilherme <guilherme182@gmail.com>
2014-07-27 16:47:34 -0300

    Task 1.07 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -11,14 +11,17 @@ public class MyCoffeeMachine implements CoffeeMachine {
 	private ComponentsFactory factory;	
 	private int value;
 	private Coin coin;
+	private ArrayList<Coin> coins;
 	
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		this.factory = factory;
 		this.factory.getDisplay().info("Insert coins and select a drink!");
+		this.coins = new ArrayList<Coin>();
 	}
 
 	public void insertCoin(Coin dime) throws CoffeeMachineException {
 		try {
+			this.coins.add(dime);
 			this.coin = dime;
 			this.value += dime.getValue();	
 			this.factory.getDisplay().info("Total: US$ " + this.value / 100 + "." + this.value % 100);
@@ -30,9 +33,18 @@ public class MyCoffeeMachine implements CoffeeMachine {
 	public void cancel() throws CoffeeMachineException {
 		if(this.value == 0) 
 			throw new CoffeeMachineException("No coin has been inserted");
-		
-		this.factory.getDisplay().warn(Messages.CANCEL_MESSAGE);
-		this.factory.getCashBox().release(coin);
+		if(this.coins.size() != 0) {
+			Coin.reverse();
+			this.factory.getDisplay().warn(Messages.CANCEL_MESSAGE);
+			for(Coin c : this.coins) {
+				for(Coin cc : this.coins) {
+					if(c != cc) 
+						this.factory.getCashBox().release(cc);
+					else
+						this.factory.getCashBox().release(c);
+				}
+			}
+		}		
 		this.factory.getDisplay().info (Messages.INSERT_COINS_MESSAGE);		
 	}
 }
