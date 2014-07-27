e279dc4045db625b559be5055bdc6d11b9f15dee
Guilherme <guilherme182@gmail.com>
2014-07-27 14:46:06 -0300

    Task 1.03 finished for TECNOLOGIA

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,18 +1,22 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
+import java.util.ArrayList;
+
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 
 public class MyCoffeeMachine implements CoffeeMachine {
-	private ComponentsFactory factory;
-
+	private ComponentsFactory factory;	
+	private int value;
+	
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		this.factory = factory;
 		this.factory.getDisplay().info("Insert coins and select a drink!");
 	}
 
 	public void insertCoin(Coin dime) {
-		this.factory.getDisplay().info("Total: US$ " + dime.getValue() / 100 + "." + dime.getValue() % 100);
+		this.value += dime.getValue();	
+		this.factory.getDisplay().info("Total: US$ " + this.value / 100 + "." + this.value % 100);
 	}
 }
