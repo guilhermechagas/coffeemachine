65e8160ea6eab2c3b27b036e13a5ad342f21cd08
Guilherme <guilherme182@gmail.com>
2014-07-28 09:53:49 -0300

    Task 1.12 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -66,6 +66,7 @@ public class MyCoffeeMachine implements CoffeeMachine {
 		this.factory.getDisplay().info(Messages.TAKE_DRINK);
 		this.factory.getDisplay().info(Messages.INSERT_COINS);
 		this.coins.clear();
+		
 	}
 }
 
